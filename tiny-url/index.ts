611f135e4603891862d6a531fd25fb1d9227e823
Leon Li <lizyang@amazon.com>
2020-10-22 23:10:13 -0700

    Create the tiny url demo repo

---
+++ tiny-url/index.ts
@@ -0,0 +1,97 @@
+import apigateway = require("@aws-cdk/aws-apigateway");
+import dynamodb = require("@aws-cdk/aws-dynamodb");
+import lambda = require("@aws-cdk/aws-lambda");
+import cdk = require("@aws-cdk/core");
+
+export class TinyUrlStack extends cdk.Stack {
+  constructor(app: cdk.App, id: string) {
+    super(app, id);
+
+    // Setup the dynamodb table
+    const dynamodbTable = new dynamodb.Table(this, "TinyUrls", {
+      partitionKey: {
+        name: "tinyUrl",
+        type: dynamodb.AttributeType.STRING,
+      },
+      tableName: "TinyUrls",
+      // Retain will not attempt to delete the new table.
+      // It will remain in your account until manually deleted.
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    // Setup the lambda
+    const createTinyUrlLambda = new lambda.Function(this, "CreateTinyUrlLambda", {
+      code: new lambda.AssetCode("src"),
+      handler: "tiny_url.handler",
+      runtime: lambda.Runtime.PYTHON_3_8,
+      environment: {
+        TABLE_NAME: dynamodbTable.tableName,
+        PRIMARY_KEY: "tinyUrl",
+      },
+    });
+
+    const describeTinyUrlLambda = new lambda.Function(this, "DescribeTinyUrlLambda", {
+      code: new lambda.AssetCode("src"),
+      handler: "tiny_url.handler",
+      runtime: lambda.Runtime.PYTHON_3_8,
+      environment: {
+        TABLE_NAME: dynamodbTable.tableName,
+        PRIMARY_KEY: "tinyUrl",
+      },
+    });
+
+    dynamodbTable.grantReadWriteData(createTinyUrlLambda);
+    dynamodbTable.grantReadData(describeTinyUrlLambda);
+
+    // Setup the API
+    const api = new apigateway.RestApi(this, "TinyUrlApi");
+    const tinyUrls = api.root.addResource("tiny_urls");
+    tinyUrls.addMethod("POST", new apigateway.LambdaIntegration(createTinyUrlLambda));
+    addCorsOptions(tinyUrls);
+
+    const tinyUrl = tinyUrls.addResource("{tinyUrl}");
+    tinyUrl.addMethod("GET", new apigateway.LambdaIntegration(describeTinyUrlLambda));
+    addCorsOptions(tinyUrl);
+  }
+}
+
+function addCorsOptions(apiResource: apigateway.IResource) {
+  apiResource.addMethod(
+    "OPTIONS",
+    new apigateway.MockIntegration({
+      integrationResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers":
+              "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Amz-User-Agent'",
+            "method.response.header.Access-Control-Allow-Origin": "'*'",
+            "method.response.header.Access-Control-Allow-Credentials": "'false'",
+            "method.response.header.Access-Control-Allow-Methods": "'OPTIONS,GET,PUT,POST,DELETE'",
+          },
+        },
+      ],
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        "application/json": "{\"statusCode\": 200}",
+      },
+    }),
+    {
+      methodResponses: [
+        {
+          statusCode: "200",
+          responseParameters: {
+            "method.response.header.Access-Control-Allow-Headers": true,
+            "method.response.header.Access-Control-Allow-Methods": true,
+            "method.response.header.Access-Control-Allow-Credentials": true,
+            "method.response.header.Access-Control-Allow-Origin": true,
+          },
+        },
+      ],
+    }
+  );
+}
+
+const app = new cdk.App();
+new TinyUrlStack(app, "TinyUrlStack");
+app.synth();
---
+++ tiny-url/src/tiny_url.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = async (event = {}) => {
+    return { statusCode: 200, body: `Hello world ${event}` };
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGlueV91cmwuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyJ0aW55X3VybC50cyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOztBQUFhLFFBQUEsT0FBTyxHQUFHLEtBQUssRUFBRSxRQUFhLEVBQUUsRUFBZ0IsRUFBRTtJQUM3RCxPQUFPLEVBQUUsVUFBVSxFQUFFLEdBQUcsRUFBRSxJQUFJLEVBQUUsZUFBZSxLQUFLLEVBQUUsRUFBRSxDQUFDO0FBQzNELENBQUMsQ0FBQyIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBjb25zdCBoYW5kbGVyID0gYXN5bmMgKGV2ZW50OiBhbnkgPSB7fSk6IFByb21pc2U8YW55PiA9PiB7XG4gIHJldHVybiB7IHN0YXR1c0NvZGU6IDIwMCwgYm9keTogYEhlbGxvIHdvcmxkICR7ZXZlbnR9YCB9O1xufTtcbiJdfQ==
