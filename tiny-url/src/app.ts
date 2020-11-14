1e8f990570f09c556ef27d32e4547af906040180
Leon Li <lizyang@amazon.com>
2020-11-14 14:36:06 -0800

    Implement the conditional check

--- tiny-url/src/app.ts
+++ tiny-url/src/app.ts
@@ -8,8 +8,10 @@ const BASE62_STR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567
 const BASE = 62;
 
 const tinyUrlTable: string = process.env.TABLE_NAME ? process.env.TABLE_NAME : "DEFAULT";
-// const tinyUrlTablePrimaryKey: string = process.env.PRIMARY_KEY ? process.env.PRIMARY_KEY : "DEFAULT";
-// const longUrlKey = "longUrl";
+const TINY_URL_KEY: string = process.env.PRIMARY_KEY ? process.env.PRIMARY_KEY : "tinyUrl";
+const LONG_URL_KEY = "longUrl";
+const REF_COUNT_KEY = "refCount";
+const REF_COUNT = "%count";
 
 export const app = express();
 const router = express.Router();
@@ -24,25 +26,62 @@ router.post("/tiny_urls", async (req: express.Request, res: express.Response) =>
     const dynamodb: DynamoDBClient = new DynamoDBClient({ region: "us-west-2" });
     const longUrl: string = req.body["longUrl"];
 
-    let urlInteger: number = getRandomInt();
+    let urlInteger = 0;
+    // Get next urlInteger
+    try {
+        let command: GetItemCommand | PutItemCommand = new GetItemCommand({
+            TableName: tinyUrlTable,
+            Key: {
+                [TINY_URL_KEY]: { S: REF_COUNT }
+            }
+        });
+        const result: GetItemOutput = await dynamodb.send(command);
+        if (result.Item) {
+            urlInteger = Number(result.Item[REF_COUNT_KEY].N);
+        }
+
+        console.log("Url integer is : " + urlInteger);
+        command = new PutItemCommand({
+            TableName: tinyUrlTable,
+            Item: {
+                [TINY_URL_KEY]: { S: REF_COUNT },
+                [REF_COUNT_KEY]: { N: (urlInteger + 1).toString() }
+            },
+            ConditionExpression: "attribute_not_exists(#refCount) OR #refCount = :expectedCount",
+            ExpressionAttributeNames: {
+                "#refCount": REF_COUNT_KEY
+            },
+            ExpressionAttributeValues: {
+                ":expectedCount": { N: urlInteger.toString() }
+            }
+        });
+        await dynamodb.send(command);
+    } catch (err) {
+        console.log(err);
+        throw err;
+    }
+
     let tinyUrl = "";
+    urlInteger += 1;
     while (urlInteger != 0) {
         console.log("url integer is : " + urlInteger.toString());
         tinyUrl = BASE62_STR[urlInteger % BASE] + tinyUrl;
         console.log("tiny string is : " + tinyUrl);
         urlInteger = Math.floor(urlInteger / BASE);
     }
+
     try {
         const command = new PutItemCommand({
             TableName: tinyUrlTable,
             Item: {
-                "tinyUrl": { S: tinyUrl},
-                "longUrl": { S: longUrl}
+                [TINY_URL_KEY]: { S: tinyUrl},
+                [LONG_URL_KEY]: { S: longUrl}
             }
         });
         await dynamodb.send(command);
     } catch (err) {
         console.log(err);
+        throw err;
     }
 
     res.json({"tinyUrl": tinyUrl});
@@ -64,7 +103,7 @@ router.get("/tiny_urls/:tinyUrl", async (req: express.Request, res: express.Resp
         const command = new GetItemCommand({
             TableName: tinyUrlTable,
             Key: {
-                "tinyUrl": { S: tinyUrl }
+                [TINY_URL_KEY]: { S: tinyUrl }
             }
         });
         result = await dynamodb.send(command);
@@ -74,9 +113,9 @@ router.get("/tiny_urls/:tinyUrl", async (req: express.Request, res: express.Resp
     res.json({"longUrl": result.Item ? result.Item["longUrl"] : "not found"});
 });
 
-function getRandomInt() {
-    return Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER));
-}
+// function getRandomInt() {
+//     return Math.floor(Math.random() * Math.floor(Number.MAX_SAFE_INTEGER));
+// }
 
 app.use("/", router);
 app.use((_: express.Request, res: express.Response) => {
